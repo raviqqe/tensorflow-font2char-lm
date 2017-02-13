2b95b44d9fd3db93584f2a2d293e86bd390483f5
Yota Toyama <raviqqe@gmail.com>
2017-02-13 11:05:24 +0900

    Prepare dataset

---
+++ examples/lib/common.rb
@@ -0,0 +1,27 @@
+require 'set'
+
+
+directory 'var'
+
+
+task :dataset => 'var' do |t|
+  repo_dir = File.join t.source, 'clojure'
+  sh "git clone https://github.com/clojure/clojure #{repo_dir}" unless File.directory? repo_dir
+
+  dataset_dir = File.join(t.source, 'dataset')
+  mkdir_p dataset_dir
+  Dir.glob('**/*.clj').each do |filename|
+    cp filename , File.join(dataset_dir, filename.gsub(/\//, '_'))
+  end
+end
+
+
+file 'var/chars.txt' => :dataset do |t|
+  chars = ''
+
+  Dir.glob('var/dataset/*.clj').each do |filename|
+    chars += File.read(filename).gsub(/[[:space:]]/, '').gsub(/[^[:print:]]/, '')
+  end
+
+  File.write t.name, Set.new(chars.chars).to_a.sort.join.gsub(/./, "\\0\n")
+end
